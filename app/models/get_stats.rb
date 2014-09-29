4484d6b39e2977e4a7b94f034c045f4b59433032
Scott <scsilver.umd@gmil.com>
2014-09-29 13:49:07 -0600

    get_stats update

--- app/models/get_stats.rb
+++ app/models/get_stats.rb
@@ -1,16 +1,43 @@
 class GetStats
-  def self.update_2hourly_weather
-      @resorts = Resort.all
-      @resorts.each do |r|
-        apikey = 'f8096e4064a18bec'
-        url = "http://api.wunderground.com/api/#{apikey}/conditions/q/#{r.address}.json"
-        @results = HTTParty.get url
-      
-        r.temp = @results["current_observation"]["temp_f"]
-        r.save
+def self.weather_update_1
+  @resorts = Resort.where(:id => 1..7)
+  @resorts.each do |r|
+  apikey = 'f8096e4064a18bec'
+  url = "http://api.wunderground.com/api/#{apikey}/conditions/q/#{r.address}.json"
+  @results = HTTParty.get url
+  r.temp = @results["current_observation"]["temp_f"]
+  r.save
         #r.snowfallforecast
-      end
-  end
+    end
+end
+def self.weather_update_2
+  @resorts = Resort.where(:id => 7..15)
+  @resorts.each do |r|
+  apikey = 'f8096e4064a18bec'
+  url = "http://api.wunderground.com/api/#{apikey}/conditions/q/#{r.address}.json"
+  @results = HTTParty.get url
+  r.temp = @results["current_observation"]["temp_f"]
+  r.save
+        #r.snowfallforecast
+    end
+end
+def self.weather_update_3
+  @resorts = Resort.where(:id => 15..22)
+  @resorts.each do |r|
+  apikey = 'f8096e4064a18bec'
+  url = "http://api.wunderground.com/api/#{apikey}/conditions/q/#{r.address}.json"
+  @results = HTTParty.get url
+  r.temp = @results["current_observation"]["temp_f"]
+  r.save
+        #r.snowfallforecast
+    end
+end
+  
+  
+  
+  
+  
+  
   def self.update_snow_stats
     urls = "http://www.onthesnow.com/colorado/snow-rss.html"
     feeds = Feedjira::Feed.fetch_and_parse urls
