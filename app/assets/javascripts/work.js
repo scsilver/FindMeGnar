5534536e3733a184992eacd0cbca67fe6eb8695f
Scott <scsilver.umd@gmil.com>
2014-10-24 00:58:08 -0600

    style fixups

--- app/assets/javascripts/work.js
+++ app/assets/javascripts/work.js
@@ -5,43 +5,35 @@
 ////////////////////////////////////////////
 var scroll_pos, count = 0;
 var scroll_point = 300;
-var colors =  ['#2B1C0A','#59569E','#4DB7F9','#CCF6F9','#ECEDED'];
+var colors =  ['#00A8C6','#F9F2E7','#40C0CB',,'#AEE239','#8FBE00'];
 
 ////////////////////////////////////////////
 
 //			READY
 
 ////////////////////////////////////////////
-$(function() {
+/*$(function() {
 	TweenMax.to('body', 1, {
-		backgroundColor: colors[0]
+		backgroundColor: colors[1]
 	});
 
 	$('.featured-image').addClass("hidden").viewportChecker({
-		classToAdd: 'visible animated fadeInUp', // Class to add to the elements when they are visible
+		classToAdd: 'visible animated fadeInUp', 
 		offset: 100
 	});
 });
 
-////////////////////////////////////////////
 
-//			SCROLL
-
-////////////////////////////////////////////
 $(window).scroll(function(e) {
 	scroll_pos = $(this).scrollTop();
-	$('.portfolio').each(function(i) {
-		if (scroll_pos + 160  > $('.portfolio').outerHeight() * i && scroll_pos <= $('.portfolio').height() * (i + 1)) {
-			TweenMax.to('body', 1, {
+	
+	$(colors).each(function(i) {
+		if (scroll_pos > ($('.container-fluid').outerHeight()-$(window).height())/5 * i ){
+			TweenMax.to('.container-fluid', 1, {
 				backgroundColor: colors[i]
 			});
 		}
-		if (scroll_pos - 50 < $(this).scrollTop() ){
-			TweenMax.to('body', 1, {
-				backgroundColor: colors[0]
-			});
-		}
-		
+	
 	});
 
-});
+});/*
